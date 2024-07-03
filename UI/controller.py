8e7ee2d3ca9b8171be83ae7645beefe4d7961fa9
ManueleCongiu <manuele.congiu@gmail.com>
2024-07-03 17:19:48 +0200

    Soluzione Baseball

--- UI/controller.py
+++ UI/controller.py
@@ -1,3 +1,5 @@
+import warnings
+
 import flet as ft
 
 
@@ -7,12 +9,65 @@ class Controller:
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+        self._selectedTeam = None
 
     def handleCreaGrafo(self, e):
-        pass
+        if self._view._ddAnno.value is None:
+            self._view._txtOutSquadre.controls.append(ft.Text(f"Seleziona un anno dal menu."))
+            return
+        self._model.buildGraph(self._view._ddAnno.value)
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text(f"Grafo correttamente creato"))
+        n, a = self._model.getGraphDetails()
+        self._view._txt_result.controls.append(ft.Text(f"Il grafo è costituito di {n} nodi e {a} archi"))
+        self._view.update_page()
 
     def handleDettagli(self, e):
-        pass
+        vicini = self._model.getSortedNeighbors(self._selectedTeam)
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text(f"Stampo i vicini di "
+                                                       f"{self._selectedTeam} con relativo peso dell'arco"))
+        for v in vicini:
+            self._view._txt_result.controls.append(ft.Text(f"{v[1]} - {v[0]}"))
+        self._view.update_page()
 
     def handlePercorso(self, e):
-        pass
+        if self._selectedTeam is None:
+            warnings.warn("Squadra non selezionata")
+            self._view._txt_result.controls.append(ft.Text(f"Squadra non selezionata"))
+
+        path = self._model.getPercorso(self._selectedTeam)
+
+        self._view._txt_result.controls.clear()
+        self._view._txt_result.controls.append(ft.Text(f"Percorso trovato:"))
+        for p in path:
+            self._view._txt_result.controls.append(ft.Text(f"{p[0]} -- {p[1]}"))
+        self._view.update_page()
+
+    def handleDDYearSelections(self, e):
+        teams = self._model.getTeamsOfYear(self._view._ddAnno.value)
+        self._view._txtOutSquadre.controls.clear()
+        self._view._txtOutSquadre.controls.append(ft.Text(f"Ho trovato {len(teams)} squadre che hanno"
+                                                          f" giocato nel {self._view._ddAnno.value}."))
+        for t in teams:
+            self._view._txtOutSquadre.controls.append(ft.Text(f"{t.name}"))
+            self._view._ddSquadra.options.append(ft.dropdown.Option(data=t,
+                                                                    text=t.teamCode,
+                                                                    on_click=self.readDDTeams))
+        self._view.update_page()
+
+    def readDDTeams(self, e):
+        if e.control.data is None:
+            self._selectedTeam = None
+        else:
+            self._selectedTeam = e.control.data
+
+    def fillDDYear(self):
+        years = self._model.getYears()
+        yearsDD = map(lambda x: ft.dropdown.Option(x), years)
+        self._view._ddAnno.options = yearsDD
+        self._view.update_page()
+
+        # yearsDD = []
+        # for y in years:
+        #     yearsDD.append(ft.dropdown.Option(y))
--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,72 @@
 from database.DB_connect import DBConnect
+from model.team import Team
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getAllYears():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """SELECT DISTINCT(YEAR) 
+                   FROM teams t 
+                   WHERE `year` >= 1985 
+                   ORDER BY `year` DESC """
+
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(row["YEAR"])
+
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getTeamsOfYears(year):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """SELECT * 
+                   FROM teams t 
+                   WHERE `year` = %s """
+
+        cursor.execute(query, (year,))
+
+        for row in cursor:
+            result.append(Team(**row))
+
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getSalaryOfTeams(year, idMap):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """SELECT t.teamCode, t.ID, SUM(s.salary) as totSalary
+                    FROM salaries s, teams t, appearances a
+                    WHERE s.`year` = t.`year` and t.`year` = a.`year` 
+                    AND a.`year` = %s
+                    AND t.ID = a.teamID 
+                    AND s.playerID = a.playerID 
+                    GROUP BY t.teamCode """
+
+        cursor.execute(query, (year,))
+
+        result = {}
+        for row in cursor:
+            #result.append((idMap[row["ID"]], row["totSalary"]))
+            result[idMap[row["ID"]]] = row["totSalary"]
+
+        cursor.close()
+        conn.close()
+        return result
